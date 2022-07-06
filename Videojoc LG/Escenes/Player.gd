1c4591bcf7c4f4e0048b8c01a278290f916a4068
Guillman07 <101257239+Guillman07@users.noreply.github.com>
2022-07-06 13:01:15 +0200

    V 0.1.1
    No se pq no es mou

--- Videojoc LG/Escenes/Player.gd
+++ Videojoc LG/Escenes/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-
+var velocitat = Vector2.ZERO
+var velocitat_base = 300
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,5 +13,29 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	velocitat = Vector2.ZERO
+	if Input.is_action_pressed("Est"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("Oest"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_pressed("Nord"):
+		velocitat += Vector2.UP * velocitat_base
+	if Input.is_action_pressed("Sud"):
+		velocitat += Vector2.DOWN * velocitat_base
+	
+	
+	
+		animacio(velocitat)
+	
+func animacio(velocitat):
+	
+	if velocitat.x > 0.1:
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite.flip_h = false
+	elif velocitat.x < -0.1:
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite.flip_h = true
+		
+	if abs(velocitat.x) < 0.1:
+		$AnimatedSprite.play("Idle")
