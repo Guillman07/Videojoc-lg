54616e59617c379fafe66a0c068da5f0028a1e6b
Insaly <lluc@aymerich.com>
2022-07-06 20:01:07 +0200

    V 0.1.4

--- Videojoc LG/Escenes/Player.gd
+++ Videojoc LG/Escenes/Player.gd
@@ -12,30 +12,35 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+
+func physics_process(delta):
+	velocitat.x = 0
+	velocitat.y = 0
 	
-	if Input.is_action_pressed("Est"):
+	if Input.is_action_just_pressed("Est"):
+		print("EST")
 		velocitat += Vector2.RIGHT * velocitat_base
-	if Input.is_action_pressed("Oest"):
+	if Input.is_action_just_pressed("Oest"):
+		print("OEST")
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_pressed("Nord"):
+	if Input.is_action_just_pressed("Nord"):
+		print("NORD")
 		velocitat += Vector2.UP * velocitat_base
-	if Input.is_action_pressed("Sud"):
+	if Input.is_action_just_pressed("Sud"):
+		print("SUD")
 		velocitat += Vector2.DOWN * velocitat_base
 	
-	
-	
-		animacio(velocitat)
+	move_and_slide(velocitat)
+	animacio(velocitat)
 	
 func animacio(velocitat):
 	
-	if velocitat.x > 0.1:
+	if velocitat.x > 0:
 		$AnimatedSprite.play("Run")
 		$AnimatedSprite.flip_h = false
-	elif velocitat.x < -0.1:
+	elif velocitat.x < 0:
 		$AnimatedSprite.play("Run")
 		$AnimatedSprite.flip_h = true
 		
-	if abs(velocitat.x) < 0.1:
+	elif velocitat.x == 0:
 		$AnimatedSprite.play("Idle")
