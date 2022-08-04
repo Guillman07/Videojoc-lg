51b56db498bf2608079063165fda754b80edb810
Guillman07 <101257239+Guillman07@users.noreply.github.com>
2022-08-04 19:54:48 +0200

    V 0.1.6

--- Videojoc LG/Escenes/Subdit_foc.gd
+++ Videojoc LG/Escenes/Subdit_foc.gd
@@ -1,13 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
 const Bola_foc = preload("res://Escenes/Bola_foc.tscn")
 var energia = 0
-
+var velocitat_base = 40
+var velocitat = Vector2.ZERO
 func _ready():
 	pass 
 
-#func _process(delta):
-#	pass
+func _process(delta):
+	moviment()
 #aproparse al prota per arribar al rango suicient de atac simple
 func atac_simple():
 	pass
@@ -21,3 +22,5 @@ func bola_foc():
 		Subjecte.position = $Position2D.position
 		
 # no fa gaire mal d'impacte pero si que fa bastant de quemadura.
+func moviment():
+	velocitat = move_and_slide(velocitat_base)
