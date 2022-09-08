294ab7a50e942bf60752a1bcf000069d0f028603
Insaly <lluc@aymerich.com>
2022-09-08 19:32:38 +0200

    0.1.19.1

--- Videojoc LG/Pause.gd
+++ Videojoc LG/Pause.gd
@@ -4,14 +4,8 @@ onready var ResOptionButton = $Control/Pantalla/HBoxContainer2/OptionButton
 onready var FullscreenToggle  = $Control/Pantalla/HBoxContainer/FullscreenToggle
 onready var LanguageOptionButton = $Control/HBoxContainer/Language
 
-var Resolutions: Dictionary = {"3840x2160":Vector2(3840,2160),
-								"2560x1440":Vector2(2560,1440),
-								"1920x1080":Vector2(1920,1080),
-								"1366x768":Vector2(1366,768),
-								"1536x864":Vector2(1536,864),
+var Resolutions: Dictionary = {"1920x1080":Vector2(1920,1080),
 								"1280x720":Vector2(1280,720),
-								"1440x900":Vector2(1440,900),
-								"1600x900":Vector2(1600,900),
 								"1024x600":Vector2(1024,600),
 								"800x600":Vector2(800,600)}
 var Idiomes_disp = ["English","Catala"]
@@ -65,7 +59,6 @@ func _input(event):
 func _on_OptionButton_item_selected(index):
 	var size = Resolutions.get(ResOptionButton.get_item_text(index))
 	OS.set_window_size(size)
-	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT,SceneTree.STRETCH_ASPECT_KEEP,size)
 
 func _on_FullscreenToggle_toggled(button_pressed):
 	OS.set_window_fullscreen(button_pressed)
